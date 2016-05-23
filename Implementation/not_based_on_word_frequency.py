703144ff0d2ed4725aa09937d38204e208c272ab
krzyszto9 <krzyszto9@gmail.com>
2016-05-23 23:55:48 +0200

    v1.1

--- Implementation/not_based_on_word_frequency.py
+++ Implementation/not_based_on_word_frequency.py
@@ -5,26 +5,25 @@ import zlib
 import os.path
 from collections import OrderedDict
 
-methods = (('ncd_zlib','normalized compression distance using zlib'),('ncd_lzma','normalized compression distance using lzma'),('all','use all above methods'))
+methods = 	(('ncd_zlib','normalized compression distance using zlib'),
+		('ncd_lzma','normalized compression distance using lzma'),
+		('all','use all above methods'))
 methods = OrderedDict(methods)
 
 def parse():
-    parser = argparse.ArgumentParser(description = 'Methods that do not require resolving the sequence with fixed word (oligomer) length segments', add_help=False,formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(description = 'Methods that do not require resolving the sequence with fixed word (oligomer) length segments.\nAvailable methods: \n' + '\n'.join('{:<20}: {} '.format(k, v) for k, v in methods.items())+'\nIf lzma module is not installed, normalized compression distance using lzma method is unavailable.', add_help=False,formatter_class=argparse.RawDescriptionHelpFormatter)
     required = parser.add_argument_group('Required arguments')
-    required.add_argument('--input', '-i', help='input file', required=True, type=argparse.FileType('r'))
-    required.add_argument('--methods', '-m', help='choose one or more from: ' +"'"+"', '".join(methods.keys()) +"'.\n" + '\n'.join('{}: {}'.format(k, v) for k, v in methods.items()) +'\nIf lzma module is not installed, normalized compression distance using lzma method is unavailable.', required=True, nargs='+')
+    required.add_argument('--file', '-f', help='read sequence data from FILE', required=True, type=argparse.FileType('r'))
+    required.add_argument('--method', '-m', required=True, nargs='+', choices = methods.keys(), help='choose one or more from: ' +"'"+"', '".join(methods.keys()) +"'", metavar='METHOD')    
     optional = parser.add_argument_group('Optional arguments')
     optional.add_argument('--help', '-h', action='help', help='show this help message and exit')
     optional.add_argument('--output', '-o', action='store_true', help='write output to proper file(s)')
-    optional.add_argument('--quiet', '-q', action='store_true', help='do not show any results or mesages, only if option -o/--output is set')
+    optional.add_argument('--quiet', '-q', action='store_true', help='do not show any results or messages, use this only if option -o/--output is set')
     try:
         pars = parser.parse_args()
-        pars.methods = map(str.lower,pars.methods)
-        if 'all' in pars.methods: 
+        if 'all' in pars.method: 
             del methods['all']
-            pars.methods = methods.keys()
-        elif len([i for i in pars.methods if i in methods.keys()]) != len(pars.methods):
-            parser.error( 'unrecignized method name')
+            pars.method = methods.keys()
         if pars.quiet and not pars.output:  parser.error( 'option -q/--quiet can only be used if option -o/--output is set')
         return pars
     except IOError, msg:
@@ -67,13 +66,13 @@ def ncd_lzma(seqs_list):
 
 def main():
     arguments = parse()
-    sequences_list, ids_list = get_seqs_and_ids(arguments.input.name)
-    for name in arguments.methods:
+    sequences_list, ids_list = get_seqs_and_ids(arguments.file.name)
+    for name in arguments.method:
         result = eval(name +'(sequences_list)')
         if not arguments.quiet: print '\n' + methods[name] + '\n', result 
         if arguments.output:
             np.savetxt(name+'.mat', result,fmt='%.10f', header= " ".join(ids_list))
-            if os.path.exists(name+'.mat') and not arguments.quiet: print 'File ' + name +'.mat has been saved successfully\n'
+            if os.path.exists(name+'.mat') and not arguments.quiet: print 'File ' + name +'.mat has been saved successfully'
 
 if __name__ == '__main__':
     try:
